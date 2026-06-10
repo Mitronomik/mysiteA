6c4a02f851aeecc4eac65a131f9b3615fac6f17f
VKrumin <vk.ru79@gmail.com>
2026-06-11 00:54:24 +0300

    PR 3: Lead form and Yandex Mail integration

---
+++ config/mail-config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'smtp_host' => 'smtp.yandex.ru',
+    'smtp_port' => 465,
+    'smtp_user' => '',
+    'smtp_password' => '',
+    'mail_from' => '',
+    'mail_from_name' => 'FIRST LINE',
+    'lead_email_to' => '',
+];
